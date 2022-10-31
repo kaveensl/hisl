3e3b02ced564fce34075b088706aa8d2d8af4b1b
kaveensl <103991749+kaveensl@users.noreply.github.com>
2022-10-31 16:45:45 +0530

    v

--- Hisl/settings.py
+++ Hisl/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_a15&+_0s7()=mnv#86#6wb=)cz@9!a$&jb9#@k5a%7#lx@it-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'hislhackme.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
